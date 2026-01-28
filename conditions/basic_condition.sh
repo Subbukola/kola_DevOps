16de140044cf6d643132f12281d555e7d112fbcf
subbu <subbu@mail.com>
2026-01-28 21:26:28 +0530

    gt_lt

--- conditions/basic_condition.sh
+++ conditions/basic_condition.sh
@@ -4,7 +4,7 @@
 echo "ENter a number--> "
 read num
 
-if(num %2==0); then
+if((num %2==0)); then
     echo "$num is even number"
 else
     echo "$num is odd"
---
+++ conditions/gt_lt_condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " enter number"
+read num
+
+if ((num -gt 20)): then
+    echo " $num is greater than 20 "
+elif ((num -eq 20 )); then
+    echo " $num is equal to 20 "
+else    
+    echo " $num is less than 20 "
+fi
