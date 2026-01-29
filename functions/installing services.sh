aa3f0798399e86edf35e5a55a48ba25a230d263a
subbu <subbu@mail.com>
2026-01-29 22:07:23 +0530

    func

---
+++ functions/installing services.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+uid=$(id -u)
+
+if ((uid !=0)); then
+    echo "run as root user"
+    exit 1
+fi
+
+dnf install  docker -y
+status=$?
+
+if ((status !=0)); then
+    echo "exit code $status, means failure"
+    exit 1
+
+else
+    echo "status code $status, means successfull"
+
+fi
+
+dnf install  nginx -y
+status=$?
+
+if ((status !=0)); then
+    echo "exit code $status, means failure"
+    exit 1
+
+else
+    echo "status code $status, means successfull"
+
+fi
