dc6e13f8179da6e02b9f59ae81c888e40e0ab94f
subbu <subbu@mail.com>
2026-01-30 00:13:10 +0530

    functtion

---
+++ loops/cheking_already_installed.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+directory="/var/log/scrip-logs"
+file_path=$directory"/$0.sh"
+status=$?
+
+uid=$(id -u)
+
+if ((uid !=0)); then
+    echo "run as root user" | tee -a $file_path
+    exit 1
+fi
+mkdir -p $directory
+
+
+validate(){
+    if ((status !=0)); then
+    echo "exit code $status, means failure" | tee -a $file_path
+    exit 1
+
+else
+    echo "status code $status, means successfull" | tee -a $file_path
+
+fi
+
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$file_path
+    if ($? !=0); then
+        echo "$package is not installed"   
+        dnf install  $package -y &>>$file_path
+        validate $status "Installing $package "   
+
+
+     else
+        echo "$package is already installed"  
+    
+done
+
+
+dnf install  nginx -y &>>$file_path
+validate $status "Installing nginx "
+#status=$?
+
+#output--> sudo less /path.sh
