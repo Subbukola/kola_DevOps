6c396e8b3ea57bdd541ac1567172055d8183dabc
subbu <subbu@mail.com>
2026-01-29 22:28:33 +0530

    funct

---
+++ functions/instaling_service_with_input.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+uid=$(id -u)
+
+if ((uid !=0)); then
+    echo "run as root user"
+    exit 1
+fi
+
+
+status=$?
+echo "enter servicename:->"
+read servicename
+
+
+validate(){
+    if ((status !=0)); then
+    echo "exit code $status, means failure"
+    exit 1
+
+else
+    echo "status code $status, means successfull"
+
+fi
+
+}
+
+
+dnf install  $servicename -y
+validate $status "Installing docker" 
+
+#dnf install  nginx -y
+#validate $status "Installing nginx "
+#status=$?
