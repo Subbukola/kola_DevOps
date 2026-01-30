da8ee7f0a2e27bad5fbaad3dd30d6b35b6726e05
subbu <subbu@mail.com>
2026-01-30 15:13:35 +0530

    error

---
+++ error_handling.sh/set.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+directory="/var/log/scrip-logs"
+file_path=$directory"/$0.sh"
+status=$?
+
+set -e # checks error . if found it will exit. return error code ERR to kernal in background
+uid=$(id -u)
+
+if ((uid !=0)); then
+    echo "run as root user" | tee -a $file_path
+    exit 1
+fi
+mkdir -p $directory
+
+
+validate(){
+    if ((status !=0)); then
+    echo "exit code $status, means failure" | tee -a $file_path
+    exit 1
+
+else
+    echo "status code $status, means successfull" | tee -a $file_path
+
+fi
+
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$file_path
+    if ($? !=0); then
+        echo "$package is not installed"   
+        dnf install  $package -y &>>$file_path
+        validate $status "Installing $package "   
+
+    else
+        echo "$package is already installed"  
+    fi
+
+done
+dnf install  nginx -y &>>$file_path
+#validate $status "Installing nginx "
+#status=$?
+
+#output--> sudo less /path.sh
