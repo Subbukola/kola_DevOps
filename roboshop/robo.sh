c3c4cb8846483744a67c33f2b103aeb736f5b68d
subbu <subbu@mail.com>
2026-02-01 15:45:15 +0530

    ec2

--- roboshop/robo.sh
+++ roboshop/robo.sh
@@ -7,11 +7,19 @@ SECURITY_GROUP_ID=sg-0905f25e83735d3d2
 for INSTANCE in $@
 do
 
-    aws ec2 run-instances \
+    INSTANCE_ID= $(aws ec2 run-instances \
         --image-id $IMAGE_ID \
         --instance-type t3.micro \
         --security-group-ids $SECURITY_GROUP_ID \
+        --query 'Instances[0].InstanceId' \
         --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCE'}]'
+        --output text
+        )
+
+    echo "Instance ID = '$Instance_Id'"    
 
 done
     
+#aws ec2 describe-instances --instance-ids <your_instance_id> \
+ #--query 'Reservations[*].Instances[*].PublicIpAddress' \
+#  --output text
