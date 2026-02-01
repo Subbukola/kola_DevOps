4bcbd2f91895a23b0d380eec088f6a81ee9ea77f
subbu <subbu@mail.com>
2026-02-01 14:53:00 +0530

    trap

--- error_handling/trap.sh
+++ error_handling/trap.sh
@@ -17,17 +17,17 @@ fi
 mkdir -p $directory
 
 
-validate(){
-    if (($1 != 0)); then
-    echo "exit code $1, means $2 installation failure" | tee -a $file_path
-    exit 1
+#validate(){
+ #   if (($1 != 0)); then
+  #  echo "exit code $1, means $2 installation failure" | tee -a $file_path
+ #   exit 1
+#
+#lse
+ #   echo "status code $1, so $2 installationmeans successfull" | tee -a $file_path
 
-else
-    echo "status code $1, so $2 installationmeans successfull" | tee -a $file_path
+#fi
 
-fi
-
-}
+#}
 
 for package in $@
 do
@@ -35,7 +35,7 @@ do
     if (($? !=0)); then
         echo "$package is not installed" | tee -a $file_path   
         dnf install  $package -y &>>$file_path
-        validate $? "Installing $package " | tee -a $file_path  
+      #  validate $? "Installing $package " | tee -a $file_path  
 
     else
         echo "$package is already installed"  | tee -a $file_path
