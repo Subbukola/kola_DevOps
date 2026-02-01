c57adf6c36a8fcb5d91c62a5bf2796e9d54533e5
subbu <subbu@mail.com>
2026-02-01 15:31:39 +0530

    ec2

--- roboshop/robo.sh
+++ roboshop/robo.sh
@@ -4,11 +4,14 @@ IMAGE_ID=ami-0220d79f3f480ecf5
 SECURITY_GROUP_ID=sg-0905f25e83735d3d2
 
 
+for INSTANCE in $@
+do
 
+    aws ec2 run-instances \
+        --image-id $IMAGE_ID \
+        --instance-type t3.micro \
+        --security-group-ids $SECURITY_GROUP_ID \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='$INSTANCE'}]'
 
-aws ec2 run-instances \
-    --image-id $IMAGE_ID \
-    --instance-type t3.micro \
-    --security-group-ids $SECURITY_GROUP_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyDemoInstance}]'
+done
     
