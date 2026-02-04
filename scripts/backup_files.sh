628ea81690aba5aadadf994aad48f3747d7a5bfd
subbu <subbu@mail.com>
2026-02-04 21:21:21 +0530

    commited

--- scripts/backup_files.sh
+++ scripts/backup_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-user_id="$(uid)"
+user_id="$(id)"
 R='\033[0;31m'
-N='\033[0;37m
+N='\033[0;37m'
 
 if [$user_id -ne 0]; then
     echo "$R You are not authorized to perform this task $N"
