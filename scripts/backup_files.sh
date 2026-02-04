7e768e8b8a426b6965c759f6ca45067c4c583f7e
subbu <subbu@mail.com>
2026-02-04 21:26:40 +0530

    commited

--- scripts/backup_files.sh
+++ scripts/backup_files.sh
@@ -3,8 +3,14 @@
 user_id="$(id -u)"
 R='\033[0;31m'
 N='\033[0;37m'
+G='\033[0;32m'
 
 if [ $user_id -ne 0 ]; then
     echo "$R You are not authorized to perform this task $N"
+
+else
+
+    echo "$G You are  authorized to perform this task $N"
+
 
 fi
