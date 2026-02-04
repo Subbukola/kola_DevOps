ebe44c173bd3602cdaed3b4990b7aa34ff5a84b0
subbu <subbu@mail.com>
2026-02-04 22:25:23 +0530

    commited

--- scripts/backup_files.sh
+++ scripts/backup_files.sh
@@ -10,7 +10,7 @@ Y="\e[33m"
 source_dir="/var/log/backup"
 destination_dir="/home/root/backup_logs"
 
-days=${3:-14}
+days=${1:-14}
 
 #step-1 --> to check user is root or not
 
@@ -30,16 +30,28 @@ mkdir -p "$destination_dir"
 
 FILES=$(find "$source_dir" -name "*.log" -type f -mtime +$days)
 
-if [ -z $FILES]; then
+if [ -z "$FILES" ]; then
     echo -e " ${R} no files to take backup${N} "
     exit 1
 else
     echo -e  " ${G} --- FILES FOUND FOR BACKUP--- ${N} "
     timestamp=$(date +%F_%H-%M-%S)
-    Backup_zip_files="$destination_dir/app_log.tar.gz"
+    Backup_zip_files="$destination_dir/app_log$timestamp.tar.gz"
     echo -e " ${Y} archieving files ${N}"
-    tar -zcvf $Backup_zip_files$(find $source_dir -type f -name " *.log" -mtime $days )
+    tar -zcvf $Backup_zip_files $FILES
 fi
+
+#-----------------------------------------------------
+
+#check stats code and see output
+if [ $? ne 0]; then
+    echo -e " ${R} looks something is wrong ${N}"
+
+else
+    echo -e " ${R} backup done ${N}"
+fi
+
+
 
 echo "$destination_dir"
 #-----------------------------------------------------------------------------
