0f0994f65cd7f1ac718bf2400ddccf1184ef7f93
subbu <subbu@mail.com>
2026-02-04 21:41:40 +0530

    commited

--- scripts/backup_files.sh
+++ scripts/backup_files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 user_id="$(id -u)"
+
 R="\e[31m"
 N="\e[0m"
 G="\e[32m"
 Y="\e[33m"
 
+source_dir="/var/log/backup"
+destination_dir="/home/root/backup_logs"
+
+#step-1 --> to check user is root or not
 
 if [ $user_id -ne 0 ]; then
     echo -e " ${R} You are not authorized to perform this task  ${N} "
@@ -16,3 +21,7 @@ else
 
 
 fi
+#----------------------------------------------------------------------------
+mkdir -p $source_dir
+mkdir -p $destination_dir
+
